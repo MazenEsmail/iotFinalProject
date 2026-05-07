ccc4a840efbb965216ac736dbfc77990e983ad96
MazenEsmail <mazen.esmail218@gmail.com>
2026-05-07 20:12:18 +0300

    connect the project to firebase

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  update,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 let mode = "auto"; // auto | manual
 let pumpStatus = "off"; // on | off
+let currentTemp = null;
+let currentHum = null;
+let currentSoil = null;
 
 // ---------- MQTT ----------
 const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt");
@@ -115,12 +125,14 @@ client.on("message", (topic, message) => {
   if (topic === "irrigation/mode") {
     mode = msg;
     console.log("Mode:", mode);
+    updateFirebase();
   }
 
   // ---------- PUMP ----------
   if (topic === "irrigation/pump") {
     pumpStatus = msg;
     console.log("Pump:", pumpStatus);
+    updateFirebase();
   }
 
   // ---------- SOIL ----------
@@ -128,6 +140,8 @@ client.on("message", (topic, message) => {
     const soil = parseInt(msg);
 
     if (isNaN(soil)) return;
+
+    currentSoil = soil;
 
     // update value
     if (soilEl) {
@@ -169,20 +183,30 @@ client.on("message", (topic, message) => {
         soilChart.update();
       }
     }
+
+    updateFirebase();
   }
 
   // ---------- TEMP ----------
   if (topic === "irrigation/temp") {
+    currentTemp = parseFloat(msg);
+
     if (tempEl) {
       tempEl.innerText = `${msg} °C`;
     }
+
+    updateFirebase();
   }
 
   // ---------- HUMIDITY ----------
   if (topic === "irrigation/humidity") {
+    currentHum = parseFloat(msg);
+
     if (humEl) {
       humEl.innerText = `${msg} %`;
     }
+
+    updateFirebase();
   }
 });
 
@@ -195,3 +219,32 @@ function send(topic, msg) {
     console.log("MQTT Not Connected");
   }
 }
+
+//----------- FIREBASE ----------
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "iot-lab-7fd31.firebaseapp.com",
+  databaseURL: "https://iot-lab-7fd31-default-rtdb.firebaseio.com",
+  projectId: "iot-lab-7fd31",
+  storageBucket: "iot-lab-7fd31.appspot.com",
+  messagingSenderId: "41748807930",
+  appId: "1:41748807930:web:e005453616461e7a494943",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// reference node in database
+const sensorRef = ref(db, "sensorData");
+function updateFirebase() {
+  update(sensorRef, {
+    soil: Number(currentSoil),
+    temp: Number(currentTemp),
+    humidity: Number(currentHum),
+    mode: mode,
+    pump: pumpStatus,
+    timestamp: Date.now(),
+  });
+}
+window.send = send;
